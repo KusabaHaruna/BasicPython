dec44e4da7b872b19363c51c9fbb0a595199af50
KusabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-03-31 20:58:01 +0900

    python入門の課題_解答

--- pi.py
+++ pi.py
@@ -1,7 +1,4 @@
-text = """
-    How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
-"""
+#問２ 文字列
 
-# TODO
-
+Text = ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving","quantum","mechanis","All","of","thy","geometry","Herr","planck","is","fairely","haed"]
+print(list(map(len, Text)))

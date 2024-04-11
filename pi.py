89c904fb411a8e562140dcae86740ae08ef2bfaa
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-04-11 23:36:17 +0900

    ＃２変更

--- pi.py
+++ pi.py
@@ -4,8 +4,4 @@
 #print(list(map(len, Text)))
 text = " How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard"
 
-#text= "orange,apple,banana,strawbery "
 print(text.replace(' ',' ').split())
-
-#print(text.split())
-      

18abf38dc7b605ea7fb2b5354b7f68a86cde1c8e
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-04-11 23:26:30 +0900

    python入門　解答＃２

--- pi.py
+++ pi.py
@@ -1,4 +1,11 @@
 #問２ 文字列
 
-Text = ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving","quantum","mechanis","All","of","thy","geometry","Herr","planck","is","fairely","haed"]
-print(list(map(len, Text)))
+#Text = ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving","quantum","mechanis","All","of","thy","geometry","Herr","planck","is","fairely","haed"]
+#print(list(map(len, Text)))
+text = " How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard"
+
+#text= "orange,apple,banana,strawbery "
+print(text.replace(' ',' ').split())
+
+#print(text.split())
+      

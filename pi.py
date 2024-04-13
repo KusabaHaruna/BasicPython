c065dbc6a593b0424008e1b28443ff548e8ee64c
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-04-13 22:20:59 +0900

    変更_3

--- pi.py
+++ pi.py
@@ -3,5 +3,14 @@
 #Text = ["How", "I", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving","quantum","mechanis","All","of","thy","geometry","Herr","planck","is","fairely","haed"]
 #print(list(map(len, Text)))
 text = " How I want a drink, alcoholic of course, after the heavy chapters involving quantum mechanics. All of thy geometry, Herr Planck, is fairly hard"
+txt=list(map(len,text.replace(","," ").replace("."," ").split()))
+print(txt)
 
-print(text.replace(' ',' ').split())
+result = "".join(map(str,txt))
+
+print(result)
+
+#print(text.splitlines())
+#print(len(text.splitlines()))
+#print([len(line) for line in text.splitlines()])
+

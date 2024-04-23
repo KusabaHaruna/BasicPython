e08ed2699a9775aec9f2e49e777dfaba6ae1ce2b
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-04-23 21:46:34 +0900

    制御構文_1

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,41 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+#a = input("aの値を入力: ")
+#b = input("bの値を入力: ")
 
 # TODO
+#prime_numbersという関数を定義
+def prime_numbers(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "は素数ではありません。")
+                break
+        else:
+            print(num, "は素数です。")
+    else:
+        print(num, "は素数ではありません。")
+
+num = int(input("aの値を入力してください:"))
+
+#prime_numbers関数を呼び出す
+prime_numbers(num)
+
+
+
+#prime_numbersという関数を定義
+def prime_numbers(num):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "は素数ではありません。")
+                break
+        else:
+            print(num, "は素数です。")
+    else:
+        print(num, "は素数ではありません。")
+
+
+num = int(input("bの値を入力してください:"))
+
+
+#prime_numbers関数を呼び出す
+prime_numbers(num)

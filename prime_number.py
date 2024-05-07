2f3ef1ad2591ffb20bd72828f7ab28ae86028dd6
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-07 23:55:12 +0900

    変更１

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,18 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+# a = input("aの値を入力: ")
+# b = input("bの値を入力: ")
 
 # TODO
+
+def sosuu(n):
+    is_prime = True # 真
+
+    # 2から n-1 までの数で割れるかどうか判断する
+    for p in range(2, n-1):
+        if n % p == 0:
+            is_prime = True # 偽
+            break
+
+    if n == 1:
+        is_prime = False
+
+    return is_prime

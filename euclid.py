f68c843e132d42bb27c9c815adf5d8fb202e2c3f
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-07 23:50:58 +0900

    変更１

--- euclid.py
+++ euclid.py
@@ -2,3 +2,27 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+
+def euclid(a, b):
+    if a > b:
+        A = a
+        B = b
+    else:
+        A = b
+        B = A
+
+    while B != 0:
+        A, B = B, A % B
+    return A
+
+def yakusuu(n):
+
+#A = euclid(a, b)
+    if n == 1:
+       return True
+    else:
+       return False
+    
+answer = yakusuu(euclid(a, b))
+print(answer)
+

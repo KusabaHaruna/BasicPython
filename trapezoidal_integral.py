199b4a0781eaaa669539699af0308cbf6d9432af
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-07 23:57:36 +0900

    変更１

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,22 @@
-from math import sin
+from math import sin, pi
+
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+def f(a, b, N):
+    h = (b - a) / N
+    answer = 0
+
+    for i in range(1, N + 1):
+        answer += (h / 2) * (sin(a + (i - 1) * h) + sin(a + i * h))
+    
+    return answer
+
+a = 0
+b = 1
+N = 100
+
+result = f(a, b, N)
+print(result)

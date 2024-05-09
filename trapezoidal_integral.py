ef1acda78db8230d8976bb1e7b77913ee5b17ed6
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-09 21:56:13 +0900

    変更2

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,22 +1,32 @@
-from math import sin, pi
+from math import sin
+import math
 
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
 
-def f(a, b, N):
+def trapezoidal_integral(f, a = 0, b = 1, N = 100):
     h = (b - a) / N
-    answer = 0
+    S = 0
 
     for i in range(1, N + 1):
-        answer += (h / 2) * (sin(a + (i - 1) * h) + sin(a + i * h))
+        S += (h / 2) * (f(a + (i - 1) * h) + f(a + i * h))
     
-    return answer
+    return S
 
-a = 0
-b = 1
-N = 100
+def q2_function(x: float) -> float:
+    return 4/(1 + x * x)
 
-result = f(a, b, N)
-print(result)
+def q3_function(x: float) -> float:
+    return ((math.pi) ** (1/2))  * (math.exp(-x ** 2))
+
+q1 = trapezoidal_integral(sin, 0, (1/2) * (math.pi), 50)
+q2 = trapezoidal_integral(q2_function, 0, 1, 100)
+q3 = trapezoidal_integral(q3_function, -100, 100, 1000)
+
+result = [q1, q2, q3]
+for i in range(3):
+    print(f"関数のq{i+1}における積分の値は、{result[i]}です。")
+    
+                                

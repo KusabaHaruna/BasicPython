2fc19526ed1dc2b1ffdc0aaf68284652cd4fd857
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-09 21:55:12 +0900

    変更2

--- prime_number.py
+++ prime_number.py
@@ -3,16 +3,31 @@
 
 # TODO
 
-def sosuu(n):
-    is_prime = True # 真
+import math
 
-    # 2から n-1 までの数で割れるかどうか判断する
-    for p in range(2, n-1):
-        if n % p == 0:
-            is_prime = True # 偽
-            break
+def is_prime_number(n: int) -> bool: #素数判定　関数
+    if n <= 1:
+        return False
+    for i in range(2, math.isqrt(n)+1):
+             if n % 1 == 0:
+                   return False
+    else:
+          return True
 
-    if n == 1:
-        is_prime = False
 
-    return is_prime
+def natural_number() -> int: #自然数を入力するまで入力を求める　関数 
+      while True:
+            try:
+                  n = int(input("自然数を入力してください :"))
+                  if n >= 0:
+                        break
+                  else:
+                        print("正しい形式で自然数を入力したください :")
+            except ValueError:
+                  print ("ValueErroが生じています。int型での入力でお願いします")
+
+      return n
+
+n = natural_number()
+result = is_prime_number(n) 
+print(f"{n}は{"素数です" if result else "素数ではありません"}")

bde9242db6389d78e2e700a005582d736d5d14d2
KUsabaHaruna <h.kusaba.24.3.1290@4nonome.com>
2024-05-09 21:53:06 +0900

    変更2

--- euclid.py
+++ euclid.py
@@ -1,27 +1,27 @@
-import math
-import random
+a = int(input("aの値を入力してください: "))
+b = int(input("bの値を入力してください: "))
 
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
 
-def euclid(a, b):
-    r = 1
-    if a < b:
-        a, b = b, a
-    while r != 0:
-        r = a % b
-        a, b =b, r
+def euclid(a, b) -> int:
+    a, b = max(a, b), min(a, b)
+    while b != 0:
+        a, b = b, a % b
     return a
-
-result = euclid(int(a),int(b))
-print(f"最大公約数は{result}です。")
-
-def is_euclid(a: int, b: int):
-    gcm = euclid(a, b)
-    if gcm == 1:
-        return True
-    else:
-        return False
     
-result2 = euclid(a, b)
-print(result2)
+print(euclid(a, b))
+    
+def mutually_prime(a, b) :
+    return euclid(a, b) == 1
+    return False
+print(mutually_prime(a, b))
+
+import random
+count = 0
+
+for _ in range(100000) :
+    x, y = random.randint(2, 100000), random.randint(2, 100000)
+    if mutually_prime(x, y) :
+        count += 1
+
+print(count / 100000)
+
